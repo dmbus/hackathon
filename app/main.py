f2b8da13af5acaff719a9b39fd1947a70b599e17
Dmitrii Bushkov <dimabushkov@gmail.com>
2026-01-30 10:37:43 +0100

    Delete 8000 port, use nginx proxy

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, words, podcasts
 from app.db.mongodb import close_mongo_connection
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(words.router, prefix="/words", tags=["words"])
